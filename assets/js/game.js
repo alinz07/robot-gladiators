991ae4f8a41bf5a7c9fca39cf8010fe657af21f8
Tony Linz <tonyllinz@gmail.com>
2021-12-10 23:42:21 -0600

    fix playerInfo.money in confirmskip

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,7 +22,7 @@ var fightOrSkip = function() {
         if (confirmSkip) {
             window.alert(playerInfo.name + " has decided to skip this fight, Goodbye!");
             //subtract money from playerMoney for skipping
-            playerInfo.money=playerInfo.money - 10;
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
 
             //reurn true if player wants to leave
             return true;
